b2c2ac098c31cffd1eeae354bf2e37e2fa3f2512
primathinks <primathinks@gmail.com>
2013-11-02 17:06:39 -0400

    removed I do

--- ido_playlist/app/controllers/playlists_controller.rb
+++
@@ -1,7 +0,0 @@
-class PlaylistsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-end
--- ido_playlist/config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-IdoPlaylist::Application.config.session_store :cookie_store, key: '_ido_playlist_session'
