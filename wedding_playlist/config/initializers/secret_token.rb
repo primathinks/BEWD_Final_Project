274756d8142c7e347049d4b114984a56ef04ea16
primathinks <primathinks@gmail.com>
2013-11-02 18:45:28 -0400

    Initial add of wedding playlist

---
+++ wedding_playlist/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeddingPlaylist::Application.config.secret_key_base = 'ac8c92a34b48a1bcc4a74c8017c409f8aae79c500da2e2882843db035ca9436ba34902100998c79274dde8b8fb0f6235f7c27e8fb6c027940391b25844bae2db'
---
+++ wedding_playlist/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WeddingPlaylist::Application.config.session_store :cookie_store, key: '_wedding_playlist_session'
