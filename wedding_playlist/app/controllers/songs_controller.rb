de673b7f4a4ba1b2db251387f73398c283f192ee
primathinks <primathinks@gmail.com>
2013-11-03 18:09:38 -0500

    Got spotify to work

--- wedding_playlist/app/controllers/songs_controller.rb
+++ wedding_playlist/app/controllers/songs_controller.rb
@@ -1,4 +1,35 @@
 class SongsController < ApplicationController
   def index
+    if !params[:q].nil?
+    @query = params[:q].gsub(" ", "%20") 
   end
+
+  	@spotify = HTTParty.get("http://ws.spotify.com/search/1/track.json?q=#{@query}")
+
+    @results = []
+
+    if @spotify["info"]["num_results"] > 0
+      @spotify["tracks"].map do |entry|
+          name = entry["name"]
+          artist = entry["artists"].last["name"]
+          album = entry["album"]["name"]
+          date = entry["album"]["released"]
+          if entry["album"]["availability"]["territories"].include? "US"
+            @results << {name: name, artist: artist, album: album, date: date}
+          end
+        end
+
+    end
+
+  end
+
+  def create
+  	@song = Song.new(:lastfm_id => params[:lastfm_id])
+  	if @song.save
+  		redirect_to root_path
+  	else
+  		redirect_to root_path
+  	end
+  end
+
 end
