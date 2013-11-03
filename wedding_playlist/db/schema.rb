7b1a95d36f1441d36909a6ec7be8b94161d06c33
primathinks <primathinks@gmail.com>
2013-11-02 21:30:59 -0400

    Added PlaylistSong model

--- wedding_playlist/db/schema.rb
+++ wedding_playlist/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131103010706) do
+ActiveRecord::Schema.define(version: 20131103011916) do
+
+  create_table "playlist_songs", force: true do |t|
+    t.integer  "playlist_id"
+    t.integer  "song_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "playlists", force: true do |t|
     t.string   "type"
