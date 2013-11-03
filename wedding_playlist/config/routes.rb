ae37e0508e11e2bbeead464263c89341ca1e7ea6
primathinks <primathinks@gmail.com>
2013-11-02 23:13:35 -0400

    Added songs controller

--- wedding_playlist/config/routes.rb
+++ wedding_playlist/config/routes.rb
@@ -2,6 +2,7 @@ WeddingPlaylist::Application.routes.draw do
   devise_for :users
   root 'welcome#index'
   resources :playlists
+  resources :songs
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
