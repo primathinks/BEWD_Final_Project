16a4b17c958bde76893777b1c6985778bc75093d
primathinks <primathinks@gmail.com>
2013-11-03 22:43:37 -0500

    Implemented iTunes search

--- wedding_playlist/app/controllers/songs_controller.rb
+++ wedding_playlist/app/controllers/songs_controller.rb
@@ -1,26 +1,9 @@
 class SongsController < ApplicationController
   def index
-    if !params[:q].nil?
-    @query = params[:q].gsub(" ", "%20") 
-  end
-
-  	@spotify = HTTParty.get("http://ws.spotify.com/search/1/track.json?q=#{@query}")
-
-    @results = []
-
-    if @spotify["info"]["num_results"] > 0
-      @spotify["tracks"].map do |entry|
-          name = entry["name"]
-          artist = entry["artists"].last["name"]
-          album = entry["album"]["name"]
-          date = entry["album"]["released"]
-          if entry["album"]["availability"]["territories"].include? "US"
-            @results << {name: name, artist: artist, album: album, date: date}
-          end
-        end
-
+    if params[:q]
+      @query = params[:q].gsub(" ", "+") 
     end
-
+    @results = ITunesSearchAPI.search(:term => @query, :country => "US", :media => "music", :limit => 20)
   end
 
   def create
