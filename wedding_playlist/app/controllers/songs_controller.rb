cd915bfa045e30ae114c867eabaf632499e03008
primathinks <primathinks@gmail.com>
2013-11-04 14:53:46 -0500

    updated safe playlist params

--- wedding_playlist/app/controllers/songs_controller.rb
+++ wedding_playlist/app/controllers/songs_controller.rb
@@ -6,20 +6,23 @@ class SongsController < ApplicationController
     @results = ITunesSearchAPI.search(:term => @query, :country => "US", :media => "music", :limit => 20)
   end
 
+  def show
+    @song = Song.find params[:id]
+  end
+
   def create
     @song = Song.new(song_params)
   	if @song.save
-  		redirect_to songs_path
-      flash[:notice] = "Song successfully created"
+  		redirect_to @song
   	else
   		redirect_to songs_path
-      flash[:notice] = "Song failed"
+      flash[:notice] = "Oops, song cannot be displayed. Try again."
   	end
   end
 
   private
   def song_params
-    params.permit(:external_id, :name, :artist, :album)
+    params.permit(:external_id, :name, :artist, :album, :duration, :thumb100, :thumb60, :itunes, :audio)
   end
 
 end
